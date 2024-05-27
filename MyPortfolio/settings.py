cf61835b35c36a737fd7c21374399e62bacc76f1
iphadan <ephadaniel177@gmail.com>
2024-05-27 15:14:46 +0300

    whitenoise

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -5,7 +5,7 @@ import os
 
 import dj_database_url
 from django.core.management.commands.runserver import Command as runserver
-# runserver.default_port = '7000' 
+runserver.default_port = '7000' 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -80,8 +81,8 @@ DATABASES = {
     }
 }
 
-# database_url=os.environ.get('DATABASE_URL')
-# DATABASES['default'] = dj_database_url.parse(database_url)
+database_url=os.environ.get('DATABASE_URL')
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
